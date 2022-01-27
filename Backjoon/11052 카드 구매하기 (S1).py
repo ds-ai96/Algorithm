4d63bf3ca354f4353c18254ff34c77c67c3928ac
JaeHyeon Choi <81815716+ds-ai96@users.noreply.github.com>
2022-01-27 14:34:01 +0900

    22.01.27 algorithm push

---
+++ Backjoon/11052 카드 구매하기 (S1).py
@@ -0,0 +1,18 @@
+"""
+BAEKJOON ALGORITHM
+Date : 2022-01-27
+Start Time : 14:17
+End Time : 
+File Name : 11052 카드 구매하기 (S1)
+URL : https://www.acmicpc.net/problem/11052
+Category : Dynamic Programming
+"""
+
+N = int(input())
+P = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j] + P[j])
+
+print(dp[N])
